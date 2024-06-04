9e195daf64c8094701e3608c593c0d7424d685db
root <root@fourthpyramid.localdomain>
2024-06-04 23:36:55 +0300

    new update

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python3
+"""
+Queries the Reddit API and returns the number of total subscribers for a given
+subreddit.
+"""
 import requests
-
-"""
-queries the reddit api and returns the number
-of total subscribers for the subreddit
-"""
 
 
 def number_of_subscribers(subreddit):
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    header = {"User-Agent": "python/(hoberton school task 16)/0.1"}
-
-    response = requests.get(url, headers=header)
-    if response.ok:
-        subscribers_count = response.json().get("data").get("subscribers")
-        return subscribers_count
-    else:
+    """
+    Queries the Reddit API and returns the number of total subscribers for a
+    given subreddit.
+    """
+    url = 'http://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {'User-Agent': 'Python/1.0(Holberton School 0x16 task 0)'}
+    response = requests.get(url, headers=headers)
+    if (not response.ok):
         return 0
+    subscriber_count = response.json().get('data').get('subscribers')
+    return subscriber_count
