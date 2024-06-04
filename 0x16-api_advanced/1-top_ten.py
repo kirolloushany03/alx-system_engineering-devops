c3a9e879268a3b27ec7ee11457d0f07b65e73119
root <root@fourthpyramid.localdomain>
2024-06-05 00:01:56 +0300

    new update

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -8,14 +8,14 @@ import requests
 
 def top_ten(subreddit):
     url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
-    headers = {'User-Agent': 'python/task 1'}
+    headers = {"User-Agent": "python/task 1"}
 
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
         if response.status_code == 200:
-            top_data= response.json()['data']['children']
+            top_data = response.json()["data"]["children"]
             for post in top_data:
-                print(post.get('data', {}).get('title'))
+                print(post.get("data", {}).get("title"))
         else:
             print(None)
     except Exception as e:
